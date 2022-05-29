c1ff0f466dd7e6eb25400d4612ca3b938f904634
Developer Kabir <hakabir40@gmail.com>
2022-05-29 06:16:19 +0600

    server working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
@@ -10,6 +11,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.hkeao.mongodb.net:27017,cluster0-shard-00-01.hkeao.mongodb.net:27017,cluster0-shard-00-02.hkeao.mongodb.net:27017/?ssl=true&replicaSet=atlas-7i6kd9-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+      await client.connect();
+      const partsCollection = client.db('autoparts').collection('tools');
+      
+  
+      
+      // get
+      app.get('/parts', async (req, res) => {
+        const query = {};
+        const cursor = partsCollection.find(query);
+        const parts = await cursor.toArray();
+        res.send(parts);
+    });
+
+      
+  
+    }
+    finally {
+  
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
