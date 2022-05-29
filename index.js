948efc61f43ded0290069dcae98092ba93167e75
Developer Kabir <hakabir40@gmail.com>
2022-05-29 06:34:12 +0600

    jwt working

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+
+
 async function run() {
     try {
       await client.connect();
